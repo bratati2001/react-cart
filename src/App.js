b2f7a7f015be43476ab152d5877146606e8fab0b
Bratati Das <105907264+bratati2001@users.noreply.github.com>
2022-07-18 00:55:34 +0530

    creating a basic project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import{BrowserRouter as Router, Routes, Route} from'react-router-dom';
+import Home  from './pages/Home';
+import About from './pages/About';
+
+const App = () =>{
+    return (
+        <>
+        <Router>
+            <Routes>
+                <Route path="/" element = {<Home/>}></Route>
+                <Route path="/about" element = {<About/>}></Route>
+
+            
+            </Routes>
+        </Router>
+        </>
+    )
+}
+export default App;
